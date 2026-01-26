e89780645d6c92f2dbea0e80299545dce9402bf7
MTCodes01 <sreedevss05@gmail.com>
2026-01-26 14:31:13 +0530

    v1.0.2 -> Added fix to Win + D bug

--- src/resources/GameModeDetector.cpp
+++ src/resources/GameModeDetector.cpp
@@ -60,6 +60,19 @@ bool GameModeDetector::IsWindowFullscreen(HWND hwnd) {
         return false;
     }
 
+    // Get window class name
+    wchar_t className[256];
+    if (GetClassName(hwnd, className, 256) == 0) {
+        return false;
+    }
+
+    // Ignore Desktop and Taskbar
+    if (wcscmp(className, L"WorkerW") == 0 || 
+        wcscmp(className, L"Progman") == 0 || 
+        wcscmp(className, L"Shell_TrayWnd") == 0) {
+        return false;
+    }
+
     // Get window rectangle
     RECT windowRect;
     if (!GetWindowRect(hwnd, &windowRect)) {
@@ -75,12 +88,13 @@ bool GameModeDetector::IsWindowFullscreen(HWND hwnd) {
         return false;
     }
 
-    // Check if window covers entire monitor
+    // Check if window covers entire monitor (with small tolerance)
+    const int tolerance = 2; // Allow small mismatch
     bool coversMonitor = 
-        (windowRect.left <= monitorInfo.rcMonitor.left) &&
-        (windowRect.top <= monitorInfo.rcMonitor.top) &&
-        (windowRect.right >= monitorInfo.rcMonitor.right) &&
-        (windowRect.bottom >= monitorInfo.rcMonitor.bottom);
+        (windowRect.left <= monitorInfo.rcMonitor.left + tolerance) &&
+        (windowRect.top <= monitorInfo.rcMonitor.top + tolerance) &&
+        (windowRect.right >= monitorInfo.rcMonitor.right - tolerance) &&
+        (windowRect.bottom >= monitorInfo.rcMonitor.bottom - tolerance);
 
     if (!coversMonitor) {
         return false;
@@ -91,16 +105,17 @@ bool GameModeDetector::IsWindowFullscreen(HWND hwnd) {
     LONG exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
 
     // Fullscreen windows typically:
-    // - Don't have WS_OVERLAPPEDWINDOW style
+    // - Don't have WS_OVERLAPPEDWINDOW style (or at least no caption/thickframe)
     // - Have WS_POPUP style
     // - Often have WS_EX_TOPMOST extended style
     
     bool hasPopupStyle = (style & WS_POPUP) != 0;
-    bool lacksWindowStyle = (style & WS_OVERLAPPEDWINDOW) != WS_OVERLAPPEDWINDOW;
+    bool noCaption = (style & WS_CAPTION) != WS_CAPTION;
     bool isTopmost = (exStyle & WS_EX_TOPMOST) != 0;
 
     // Consider it fullscreen if it covers the monitor and has appropriate styles
-    return coversMonitor && (hasPopupStyle || lacksWindowStyle || isTopmost);
+    // Relaxed check: just covering monitor + popup OR no caption is usually enough
+    return coversMonitor && (hasPopupStyle || noCaption || isTopmost);
 }
 
 } // namespace PixelMotion
